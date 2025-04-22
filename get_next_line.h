82da64d4a6d5ae4af72a658a4e569abd70a467f2
Armen90210 <150281639+Menchna@users.noreply.github.com>
2025-04-22 15:07:05 +0400

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -11,9 +11,9 @@
 # define MAX_FD 1024
 
 char *get_next_line(int fd);
-char *ft_strjoin(char *s1, char *s2);
+char *ft_strjoin(char const *s1, char const *s2);
 size_t ft_strlen(const char *s);
 char *ft_strchr(const char *s, int c);
-char *ft_substr(char *s, unsigned int start, size_t len);
+char *ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
